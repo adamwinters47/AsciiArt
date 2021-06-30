88d8f9d4dd97a657e121cb095c23a538ce799a95
adamwinters47 <adamwinters47@gmail.com>
2021-06-29 21:51:15 -0500

    Basic Functionality
    Finish basic functionality of converting image to ascii text & writing
    that text to a file

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
+import math
+
 from PIL import Image
 import numpy as np
 
 
 def process_image():
     try:
-        im = Image.open("ImageInput/mario_test.jpg")
+        im = Image.open("ImageInput/starspittle-gmail-com.jpg")
         print(f'File {im.filename} to be processed')
         print(f'Height: {im.height}px \nWidth: {im.width}px')
-
-        pixel_matrix = np.asarray(im)
-        # for row in pixel_matrix:
-        #     for pixel in row:
-        #         print(f'Pixel: {pixel}')
-        #         for value in pixel:
-        #             print(f'Value: {value}')
+        new_im = im.resize((math.floor(im.width/4), math.floor(im.height/4)))
+        print(f'{new_im.size}')
+        pixel_matrix = np.asarray(new_im)
 
         # determine brightness of each pixel
-
+        ascii_text = '`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$'
+        divisor = 255 / 65
+        f = open('test.txt', "w+")
         for row in pixel_matrix:
+            f.write('\n')
             for pixel in row:
                 rgb_sum = 0
                 for value in pixel:
                     rgb_sum += value
                 brightness = rgb_sum / 3
-                print(f'Brightness of pixel {pixel} is {brightness}')
-
+                f.write("%s" % ascii_text[math.floor(brightness/divisor)-8]*3)
+        f.close()
+                # print(f'Brightness of pixel {pixel} is {brightness}')
         # tie brightness to an ascii character
-        '`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$'
+
         # put ascii character into grid
 
         # display image
